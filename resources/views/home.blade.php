370d39810abf094d3836388e2b5297ecb75f6080
David Gaitan <jdavid.gaitan@gmail.com>
2022-10-31 14:19:24 -0600

    Showing initial user info

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,13 +11,35 @@
                 </div>
             </div>
         @endif
-        <div class="col-md-4">
+        <div class="col-md-3">
             <div class="card">
                 <div class="card-header">My Profile</div>
                 <div class="card-body">
-                    <div class="mb-3">
-                        <strong class="d-block">Name:</strong>
-                        <p class="fs-4">{{ $user->name }}</p>
+                    <div class="row">
+                        <div class="col-12 col-md-6">
+                            <div class="mb-3">
+                                <strong class="d-block text-muted">First Name:</strong>
+                                <p class="fs-4">{{ $user->name }}</p>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <div class="mb-3">
+                                <strong class="d-block text-muted">Last Name:</strong>
+                                <p class="fs-4">{{ $user->last_name }}</p>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="mb-3">
+                                <strong class="d-block text-muted">Email:</strong>
+                                <p class="fs-4">{{ $user->email }}</p>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="mb-3">
+                                <strong class="d-block text-muted">Last Login At:</strong>
+                                <p class="fs-5">{{ $user->last_login->format('F j, Y @ h:i a') }}</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
