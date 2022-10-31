61bdc5fd894a79673fbd473b652f34eb5bc8cc10
David Gaitan <jdavid.gaitan@gmail.com>
2022-10-31 17:15:09 -0600

    adding post relationship

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,26 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
-    //
+    /**
+     * Fillable Fields
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'title', 'content', 'author_id'
+    ];
+
+    /**
+     * Post author
+     *
+     * @return BelongsTo
+     */
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
