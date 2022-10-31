62989108cacf36fe2ea0847a643422f99a9c6da1
David Gaitan <jdavid.gaitan@gmail.com>
2022-10-31 14:15:59 -0600

    upgrading to twbs 5

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,19 +2,23 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row">
+        @if (session('status'))
+            <div class="col-12">
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    {{ session('status') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            </div>
+        @endif
+        <div class="col-md-4">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
+                <div class="card-header">My Profile</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <div class="mb-3">
+                        <strong class="d-block">Name:</strong>
+                        <p class="fs-4">{{ $user->name }}</p>
+                    </div>
                 </div>
             </div>
         </div>
