80cd44ba941498cb635ab702a3febe5e4dd06d6b
David Gaitan <jdavid.gaitan@gmail.com>
2022-10-31 13:03:35 -0600

    Separating user permissions logic in a single trait

---
+++ app/Traits/HasRoles.php
@@ -0,0 +1,198 @@
+<?php
+
+namespace App\Traits;
+
+use Exception;
+
+trait HasRoles
+{
+
+    /**
+     * User Types or Level
+     */
+    public static $blogger = 3;
+    public static $supervisor = 2;
+    public static $admin = 1;
+
+    /**
+     * Make a user a blogger
+     *
+     * @return self
+     */
+    public function makeBlogger(): self
+    {
+        return $this->changeUserType(self::$blogger);
+    }
+
+    /**
+     * Make a user a Supervisor
+     *
+     * @return self
+     */
+    public function makeSupervisor(): self
+    {
+        return $this->changeUserType(self::$supervisor);
+    }
+
+    /**
+     * Make a user an admin
+     *
+     * @return self
+     */
+    public function makeAdmin(): self
+    {
+        return $this->changeUserType(self::$admin);
+    }
+
+    /**
+     * Is the current user an admin?
+     *
+     * @return boolean
+     */
+    public function isAdmin(): bool
+    {
+        return $this->user_type === self::$admin;
+    }
+
+    /**
+     * Is the user a supervisor?
+     *
+     * @return boolean
+     */
+    public function isSupervisor(): bool
+    {
+        return $this->user_type === self::$supervisor;
+    }
+
+    /**
+     * Is the user a Blogger?
+     *
+     * @return boolean
+     */
+    public function isBlogger(): bool
+    {
+        return $this->user_type === self::$blogger;
+    }
+
+    /**
+     * Has the user permission to?
+     *
+     * @param string $permission
+     * @return boolean
+     */
+    public function hasPermissionTo(string $permission): bool
+    {
+        return in_array(
+            $permission, 
+            $this->getRolePermissions($this->user_type)
+        );
+    }
+
+    /**
+     * Change user type/permission level
+     *
+     * @param integer $userType
+     * @return self
+     */
+    protected function changeUserType(int $userType): self
+    {
+        $this->validateUserType($userType);
+
+        $this->user_type = $userType;
+        $this->save();
+
+        return $this;
+    }
+
+    /**
+     * Get Role Permissions
+     *
+     * @param integer $userType
+     * @return array
+     */
+    protected function getRolePermissions(int $userType): array
+    {
+        $this->validateUserType($userType);
+
+        return [
+            self::$blogger => $this->getBloggerPermissions(),
+            self::$supervisor => $this->getSupervisorPermissions(),
+            self::$admin => $this->getAdminPermissions()
+        ][$userType];
+    }
+
+    /**
+     * Validate if a user type is valid.
+     * Will returns true if is valid,
+     * will throws and exception if invalid.
+     *
+     * @param integer $userType
+     * @return bool
+     * @throws Exception if $userType is invalid
+     */
+    protected function validateUserType(int $userType)
+    {
+        if (! in_array($userType, array_keys(self::getUserTypes()))) {
+            throw new Exception('Invalid user type! Please use a valid user type code');
+        }
+
+        return true;
+    }
+
+    /**
+     * Get Blogger Permissions
+     *
+     * @return array
+     */
+    protected function getBloggerPermissions(): array
+    {
+        return [
+            'edit_profile', 'see_my_dashboard', 'search_my_posts',
+            'see_my_posts', 'edit_my_posts', 'create_post'
+        ];
+    }
+
+    /**
+     * Get Supervisor Permissions
+     *
+     * @return array
+     */
+    protected function getSupervisorPermissions(): array
+    {
+        return [
+            ...$this->getBloggerPermissions(),
+            'see_my_bloggers', 'see_my_bloggers_posts',
+            'edit_my_bloggers_posts', 'delete_my_bloggers_posts',
+        ];
+    }
+
+    /**
+     * Get Admin Permissions
+     *
+     * @return array
+     */
+    protected function getAdminPermissions(): array
+    {
+        return [
+            ...$this->getSupervisorPermissions(),
+            'see_all_posts', 'see_user_types', 'see_users', 'create_users', 'edit_users',
+            'delete_users', 'filter_user_types', 'assign_user_types', 'delete_user',
+            'assign_supervisor', 'see_supervisors', 'see_all_posts', 'delete_any_post',
+            'edit_any_post'
+        ];
+    }
+
+    /**
+     * Retrieve the available user types
+     *
+     * @return array
+     */
+    public static function getUserTypes(): array
+    {
+        return [
+            self::$blogger => 'Blogger',
+            self::$supervisor => 'Supervisor',
+            self::$admin => 'Admin',
+        ];
+    }
+}
--- app/User.php
+++ app/User.php
@@ -2,11 +2,12 @@
 
 namespace App;
 
+use App\Traits\HasRoles;
 use Exception;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Spatie\Permission\Traits\HasRoles;
+
 
 /**
  * App/User
@@ -21,13 +22,6 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
     use Notifiable, HasRoles;
-
-    /**
-     * User Types or Lever
-     */
-    public const BLOGGER = 3;
-    public const SUPERVISOR = 2;
-    public const ADMIN = 1;
 
     /**
      * The attributes that are mass assignable.
@@ -59,36 +53,6 @@ class User extends Authenticatable
     ];
 
     /**
-     * Make a user a blogger
-     *
-     * @return User
-     */
-    public function makeBlogger(): User
-    {
-        return $this->changeUserType(self::BLOGGER);
-    }
-
-    /**
-     * Make a user a Supervisor
-     *
-     * @return User
-     */
-    public function makeSupervisor(): User
-    {
-        return $this->changeUserType(self::SUPERVISOR);
-    }
-
-    /**
-     * Make a user an admin
-     *
-     * @return User
-     */
-    public function makeAdmin(): User
-    {
-        return $this->changeUserType(self::ADMIN);
-    }
-
-    /**
      * Register user last login
      *
      * @return User
@@ -99,37 +63,5 @@ class User extends Authenticatable
         $this->save();
 
         return $this;
-    }
-
-    /**
-     * Change user type/permission level
-     *
-     * @param integer $userType
-     * @return User
-     */
-    protected function changeUserType(int $userType): User
-    {
-        if (! in_array($userType, array_keys(self::getUserTypes()))) {
-            throw new Exception('Invalid user type! Please try with a valid user type code');
-        }
-
-        $this->user_type = $userType;
-        $this->save();
-
-        return $this;
-    }
-
-    /**
-     * Retrieve the available user types
-     *
-     * @return array
-     */
-    public static function getUserTypes(): array
-    {
-        return [
-            self::BLOGGER => 'Blogger',
-            self::SUPERVISOR => 'Supervisor',
-            self::ADMIN => 'Admin',
-        ];
     }
 }
